67b639c0a7063cf2e58779c815789553afa99b46
Daniel Kim <danieljoonkimm@gmail.com>
2019-09-23 00:19:52 -0700

    [edit] 4th commit

--- index.js
+++ index.js
@@ -1,28 +1,85 @@
 
-let joongrids = (imageURLs, columns) => {
-  let images = [];
-  let splitImageString = imageURLs.split(", ");
-  let splitImageLength = splitImageString.length;
+  joongrids = (
+    options = { header, subHeader, columns: undefined, imageURLs: undefined, style: undefined }
+  ) => {
+    let header = options.header;
+    let subHeader = options.subHeader;
+    let columnNum = 12;
+    
+    if (options.columns === undefined || options.imageURLs === undefined) {
+      let images = [];
+      options.style = {
+        marginBottom: '1em'
+      }
+      options.imageURLs =
+        "https://dummyimage.com/600x400/000/fff, https://dummyimage.com/600x400/000/fff";
+      options.columns = columnNum / options.columns;
 
-  if (splitImageLength > 0) {
-    for (let i = 0; i < splitImageLength; i++) {
-      images.push({
-        image: splitImageString[i]
-      });
+      let splitImageString = options.imageURLs.split(", ");
+      let splitImageLength = splitImageString.length;
+
+      if (splitImageLength > 0) {
+        for (let i = 0; i < splitImageLength; i++) {
+          images.push({
+            image: splitImageString[i]
+          });
+        }
+
+        let finalImages = images.map((item, index) => {
+          return (
+            <div
+              className={`col-xs-${12} col-sm-${columnNum /
+                options.columns} col-md-${columnNum /
+                options.columns} col-lg-${columnNum / options.columns}`}
+                key={index}
+            >
+              <img className="img-responsive" src={item.image} alt="" style={options.style}/>
+            </div>
+          );
+        });
+
+        return <div className="row" style={{display: "flex"}}>{finalImages}</div>;
+      }
+    } else {
+      let images = [];
+      let splitImageString = options.imageURLs.split(", ");
+      let splitImageLength = splitImageString.length;
+
+      if (splitImageLength > 0) {
+        for (let i = 0; i < splitImageLength; i++) {
+          images.push({
+            image: splitImageString[i]
+          });
+        }
+
+        let finalImages = images.map((item, index) => {
+          return (
+            <div
+              className={`col-xs-${12} col-sm-${columnNum /
+                options.columns} col-md-${columnNum /
+                options.columns} col-lg-${columnNum / options.columns}`}
+                key={index}
+            >
+              <img
+                className="img-responsive"
+                src={item.image}
+                alt=""
+                style={options.style}
+              />
+            </div>
+          );
+        });
+
+        return (
+          <div className="row">
+            <h1>{header}</h1>
+            <h3>{subHeader}</h3>
+            {finalImages}
+          </div>
+        );
+      }
     }
-
-    let finalImages = images.map( (item) => {
-      return (
-        <div
-          className={`col-xs-${12} col-sm-${columns} col-md-${columns} col-lg-${columns}`}
-        >
-          <img className="img-responsive" src={item.image} alt="" />
-        </div>
-      );
-    });
-
-    return <div className="row">{finalImages}</div>;
   }
-}
+  
 
 module.exports.joongrids = joongrids;
